aeb293ab4ec1f5069a5e44b4c7246e7b82e60be0
billyFangzl <billyfang1.ui@gmail.com>
2022-11-03 15:00:37 -0400

    add main weather part

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Check city weather by openWeather API 
 
 developed by angular 12
+
+# openweathermap.org
+openweather api  
---
+++ weather-app/src/app/Components/weather-main/weather-main.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-main',
+  templateUrl: './weather-main.component.html',
+  styleUrls: ['./weather-main.component.css']
+})
+export class WeatherMainComponent implements OnInit {
+
+  weatherData:any;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getWeatherData();
+  }
+
+  getWeatherData(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=3d9dc2384d2eb80f590460b2c7510eb5')
+    .then(response=>response.json())
+    .then(data=>{this.setWeatherData(data);})
+
+    // let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
+    // this.setWeatherData(data)
+  }
+
+  setWeatherData(data: any){
+    this.weatherData = data;
+    console.log(this.weatherData);
+    let sunsetTime = new Date(this.weatherData.sys.sunset * 1000);
+    this.weatherData.sunset_time = sunsetTime.toLocaleTimeString();
+    let currentDate = new Date();
+    this.weatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
+    this.weatherData.temp_celcius = (this.weatherData.main.temp - 273.15).toFixed(0);
+    this.weatherData.temp_min = (this.weatherData.main.temp_min - 273.15).toFixed(0);
+    this.weatherData.temp_max = (this.weatherData.main.temp_max - 273.15).toFixed(0);
+    this.weatherData.temp_feels_like = (this.weatherData.main.feels_like - 273.15).toFixed(0);
+  }
+
+
+
+}
